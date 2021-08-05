56810bbd6a3cc5770a7a468912fd81e7ee933f01
Lars K.W. Gohlke <lkwg82@gmx.de>
2021-08-05 22:01:15 +0200

    moved Dockerfile for signal-cli to separate dir

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -t signal-cli .
+docker build -t signal-cli docker/signal-cli
 docker run --rm -ti \
   --user $(id -u):$(id -g) \
   --volume "$HOME"/.local/share/signal-cli:/.local/share/signal-cli \
