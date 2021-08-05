672cd797cd88b42477eb30462ae869a48cb5312a
Lars K.W. Gohlke <lkwg82@gmx.de>
2021-08-05 21:46:58 +0200

    integrated AttachmentMover

--- src/main/java/de/lgohlke/signal/attachmentdownloader/MainCommand.java
+++ src/main/java/de/lgohlke/signal/attachmentdownloader/MainCommand.java
@@ -4,21 +4,26 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import picocli.CommandLine;
 
+import java.nio.file.Path;
+
 @CommandLine.Command
 @Slf4j
 public class MainCommand implements Runnable {
-    @CommandLine.Option(names = {"-d", "--attachment-dir"}, description = "location of attachments of signal-cli", defaultValue = "$HOME/.local/share/signal-cli/attachments")
-    private String attachmentDir;
+    @CommandLine.Option(names = {"-d", "--signal-attachment-dir"}, description = "location of attachments of signal-cli", defaultValue = "$HOME/.local/share/signal-cli/attachments")
+    private String signalAttachmentDirectory;
+    @CommandLine.Option(names = {"-t", "--moved-attachment-dir"}, description = "location of attachments to moved to", required = true)
+    private String movedAttachmentDir;
     @CommandLine.Option(names = {"-h", "--help"}, usageHelp = true, description = "display a help message")
     private boolean helpRequested;
 
     @SneakyThrows
     @Override
     public void run() {
-        attachmentDir = attachmentDir.replace("$HOME", System.getenv("HOME"));
+        signalAttachmentDirectory = signalAttachmentDirectory.replace("$HOME", System.getenv("HOME"));
 
         log.info("starting reading from stdin ...");
-        log.info("search in " + attachmentDir + " for attachments");
-        new StdinDelegator(System.in, null).handle();
+        log.info("search in " + signalAttachmentDirectory + " for attachments");
+        var attachmentMover = new AttachmentMover(Path.of(signalAttachmentDirectory), Path.of(movedAttachmentDir));
+        new StdinDelegator(System.in, attachmentMover).handle();
     }
 }
