239db6e25e59c52efeabb0f05ae47ead595ab055
Lars <1891171+lkwg82@users.noreply.github.com>
2021-08-06 10:46:56 +0200

    docker build works

--- run.sh
+++ run.sh
@@ -3,6 +3,13 @@
 set -e
 
 docker build -t signal-cli docker/signal-cli
+
+docker build --target=builder -t attachment-mover -f docker/attachment-mover/Dockerfile .
+docker run --rm -ti \
+  -v "$PWD/.m2":/src/.m2 \
+  attachment-mover \
+  mvn -Dmaven.repo.local=.m2 verify -P native
+exit
 docker run --rm -ti \
   --user $(id -u):$(id -g) \
   --volume "$HOME"/.local/share/signal-cli:/.local/share/signal-cli \
--- src/test/java/de/lgohlke/signal/attachmentdownloader/mapping/NativeMappingIT.java
+++
@@ -1,9 +0,0 @@
-package de.lgohlke.signal.attachmentdownloader.mapping;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeMappingIT extends MappingTest {
-
-    // Execute the same tests but in native mode.
-}
