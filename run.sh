cdbce46692755af28044976e5c399c22ecd49a76
Lars K.W. Gohlke <lkwg82@gmx.de>
2021-08-08 21:47:05 +0200

    wip

--- run.sh
+++ run.sh
@@ -10,37 +10,21 @@ docker build -t attachment-mover-java -f docker/attachment-mover-java/Dockerfile
 # selftest
 docker run --rm -ti attachment-mover-java -h >/dev/null
 
-# binary not working, dont know why :(
-#docker build --target=builder -t attachment-mover -f docker/attachment-mover/Dockerfile .
-#docker run --rm -ti \
-#  -v "$PWD/.m2":/src/.m2 \
-#  -v "$PWD/target-c":/src/target \
-#  attachment-mover \
-#  mvn -Dmaven.repo.local=.m2 verify -P native
-
-#if [[ -n $USE_NATIVE ]]; then
-#  mvn clean verify -P native
-#  cp target/*-runner target/app
-#  upx -1v target/app
-#  target/app -h
-#  docker build --target=release -t attachment-mover -f docker/attachment-mover/Dockerfile .
-#  docker run --rm -ti attachment-mover -h
-#else
-#mvn clean verify
-#java -jar target/quarkus-app/quarkus-run.jar -h
-#fi
-
-exit
-fifo=$(mktemp)
-echo "fifo: $fifo"
-docker run --rm -ti \
+cid=$(docker run --rm -ti \
   --user $(id -u):$(id -g) \
   --volume "$HOME"/.local/share/signal-cli:/.local/share/signal-cli \
-  -e USERNAME="${USERNAME}" \
-  signal-cli | tee "${fifo}"°
+  --detach \
+  signal-cli -u"${USERNAME}" -o json receive -t -1)
 
-# docker run --rm -i \
-#    -u $(id -u):$(id -g) \
-#    -v /home/lars/.local/share/signal-cli/attachments:/signal-source \
-#    -v "$PWD"/moved_attachments:/moved_attachments \
-#    attachment-mover -d /signal-source -t /moved_attachments
+function finish {
+  echo "cleanup"
+  echo -n " killing ${cid} ... "
+  docker kill "${cid}" > /dev/null && echo "killed"
+}
+trap finish EXIT
+
+docker logs -f "${cid}" | docker run --rm -i \
+    -u $(id -u):$(id -g) \
+    -v /home/lars/.local/share/signal-cli/attachments:/signal-source \
+    -v "$PWD"/moved_attachments:/moved_attachments \
+    attachment-mover-java -d /signal-source -t /moved_attachments --debug
