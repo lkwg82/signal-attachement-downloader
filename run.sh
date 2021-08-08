a03eaa64dd0f2868683d376eade96b4708b6943a
Lars <1891171+lkwg82@users.noreply.github.com>
2021-08-08 22:19:56 +0200

    published

--- run.sh
+++ run.sh
@@ -2,29 +2,29 @@
 
 set -e
 
-docker build -t signal-cli docker/signal-cli
-# selftest
-docker run --rm -ti signal-cli -v >/dev/null
-
 docker build -t attachment-mover-java -f docker/attachment-mover-java/Dockerfile .
-# selftest
-docker run --rm -ti attachment-mover-java -h >/dev/null
 
-cid=$(docker run --rm -ti \
-  --user $(id -u):$(id -g) \
-  --volume "$HOME"/.local/share/signal-cli:/.local/share/signal-cli \
-  --detach \
-  signal-cli -u"${USERNAME}" -o json receive -t -1)
+# selftests
+docker run --rm -ti --entrypoint signal-cli attachment-mover-java -v >/dev/null
+docker run --rm -ti --entrypoint java attachment-mover-java -jar quarkus-run.jar -h >/dev/null
 
-function finish {
-  echo "cleanup"
-  echo -n " killing ${cid} ... "
-  docker kill "${cid}" > /dev/null && echo "killed"
-}
-trap finish EXIT
+if [[ -n $RELEASE ]]; then
+  timestamp=$(date "+%Y%m%d-%H%M%S")
+  docker tag attachment-mover-java lkwg82/signal-attachment-mover
+  docker tag attachment-mover-java lkwg82/signal-attachment-mover:"$timestamp"
+  docker push lkwg82/signal-attachment-mover:"$timestamp"
+  docker push lkwg82/signal-attachment-mover
+else
+  if [[ -z $USERNAME ]]; then
+    echo "ERROR pass USERNAME as env var"
+    exit 1;
+  fi
 
-docker logs -f "${cid}" | docker run --rm -i \
-    -u $(id -u):$(id -g) \
-    -v /home/lars/.local/share/signal-cli/attachments:/signal-source \
-    -v "$PWD"/moved_attachments:/moved_attachments \
-    attachment-mover-java -d /signal-source -t /moved_attachments --debug
+  docker run --rm -it \
+      -u $(id -u):$(id -g) \
+      --volume "$HOME"/.local/share/signal-cli:/home/user/.local/share/signal-cli \
+      --volume /home/lars/.local/share/signal-cli/attachments:/signal-source \
+      --volume "$PWD"/moved_attachments:/moved_attachments \
+      -e USERNAME="${USERNAME}" \
+      attachment-mover-java -d /signal-source -t /moved_attachments --debug
+fi
