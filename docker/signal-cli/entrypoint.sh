db3e04c0776da497134632ced08b202de952bcdf
Lars <1891171+lkwg82@users.noreply.github.com>
2021-08-08 21:47:05 +0200

    wip

--- docker/signal-cli/entrypoint.sh
+++ docker/signal-cli/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
-#
-#if [[ -z $USERNAME ]]; then
-#  echo "ERROR please pass USERNAME environment variable"
-#  exit 1
-#fi
 
+# shellcheck disable=SC2068
 signal-cli $@
-
-#-u"${USERNAME}" -o json receive -t -1
