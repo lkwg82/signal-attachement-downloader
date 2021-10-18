348632513be4802c1baad17a191ffc2b14ad8223
Lars K.W. Gohlke <lkwg82@gmx.de>
2021-10-18 13:20:59 +0200

    cleanup signal-cli

--- run.sh
+++ run.sh
@@ -2,14 +2,38 @@
 
 set -e
 
-docker build -t attachment-mover-java -f docker/attachment-mover-java/Dockerfile .
 
-# selftests
-docker run --rm -ti --entrypoint signal-cli attachment-mover-java -v >/dev/null
-docker run --rm -ti --entrypoint java attachment-mover-java -jar quarkus-run.jar -h >/dev/null
+# signal-cli
+(
+  cd docker/signal-cli
+  signalCliVersion=$(grep ^ARG Dockerfile | grep "SIGNAL_CLI_VERSION" | cut -d= -f2 )
+  docker build -t signal-cli .
+
+  log=$(tempfile)
+  if ! docker run --rm -ti signal-cli -v -u > "$log"; then
+    echo "ERROR signal-cli failed" >&2
+    cat "$log"
+    exit 1
+  fi
+)
+
+docker build -t attachment-mover-java -f docker/attachment-mover-java/Dockerfile .
+log=$(tempfile)
+if ! docker run --rm -ti --entrypoint java attachment-mover-java -jar quarkus-run.jar -h >"$log"; then
+  echo "ERROR app failed" >&2
+  cat "$log"
+  exit 1
+fi
 
 if [[ -n $RELEASE ]]; then
   timestamp=$(date "+%Y%m%d-%H%M%S")
+
+  signalCliVersion=$(docker history --no-trunc signal-cli | grep -v ^$ | grep SIGNAL_CLI_VERSION | cut -d= -f2 | cut -d\  -f1| xargs)
+  docker tag signal-cli lkwg82/signal-attachment-mover:signal-cli-"$signalCliVersion"
+  docker push lkwg82/signal-attachment-mover:signal-cli-"$signalCliVersion"
+  docker tag signal-cli lkwg82/signal-attachment-mover:signal-cli
+  docker push lkwg82/signal-attachment-mover:signal-cli
+
   docker tag attachment-mover-java lkwg82/signal-attachment-mover
   docker tag attachment-mover-java lkwg82/signal-attachment-mover:"$timestamp"
   docker push lkwg82/signal-attachment-mover:"$timestamp"
