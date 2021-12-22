2a3b332c8e54f36d4fcf4f16d2c20c745728e0ab
Lars <1891171+lkwg82@users.noreply.github.com>
2021-12-22 13:01:09 +0100

    adds check to signal-cli

--- docker/signal-cli/entrypoint.sh
+++ docker/signal-cli/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-set -ex
+set -e
+
+if [ -z "$USERNAME" ]; then
+  echo "missing USERNAME variable"
+  exit 1
+fi
 
 signal-cli -u "$USERNAME" --output json receive \
   | tee --append /output/messages.log
