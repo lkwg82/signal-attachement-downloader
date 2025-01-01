76abec14cd9d9ad9c7183bc9d9ea91a3aae8c281
Lars K.W. Gohlke <lkwg82@gmx.de>
2025-01-02 00:57:58 +0100

    enable multiple message logs to allow log rotation

--- docker/attachment-mover-java/entrypoint.sh
+++ docker/attachment-mover-java/entrypoint.sh
@@ -7,4 +7,8 @@ java -jar quarkus-run.jar \
   --moved-attachment-dir /moved_attachments \
   --signal-attachment-dir /signal_attachments \
   --messages-log "${MESSAGES_LOG:-/output/messages.log}" \
+  --messages-log "${MESSAGES_LOG:-/output/messages.log.1}" \
+  --messages-log "${MESSAGES_LOG:-/output/messages.log.2}" \
+  --messages-log "${MESSAGES_LOG:-/output/messages.log.3}" \
+  --messages-log "${MESSAGES_LOG:-/output/messages.log.4}" \
   --debug
