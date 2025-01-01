a7b4ba192b134fb355fa4fa1a05f7c6c314738ec
Lars K.W. Gohlke <lkwg82@gmx.de>
2025-01-01 18:42:01 +0100

    fixes error with reading message log non utf8

--- docker/attachment-mover-java/entrypoint.sh
+++ docker/attachment-mover-java/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+export JAVA_TOOL_OPTIONS="-Dfile.encoding=utf8"
+
 java -jar quarkus-run.jar \
   --moved-attachment-dir /moved_attachments \
   --signal-attachment-dir /signal_attachments \
