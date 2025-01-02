91f57a4cce35bb2e1b3cf77c31339c8b78a4ced9
Lars K.W. Gohlke <lkwg82@gmx.de>
2025-01-02 01:15:23 +0100

    fixes IDE quarkus run

--- src/main/java/de/lgohlke/signal/attachmentdownloader/ReactionHandler.java
+++ src/main/java/de/lgohlke/signal/attachmentdownloader/ReactionHandler.java
@@ -1,10 +1,12 @@
 package de.lgohlke.signal.attachmentdownloader;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.nio.file.Path;
 
+@Slf4j
 @RequiredArgsConstructor
 class ReactionHandler {
     private final Path reactionFolder;
@@ -12,20 +14,20 @@ class ReactionHandler {
     void handle(Path attachment) {
         reactionFolder.toFile().mkdirs();
 
-        System.out.println(attachment);
-        System.out.println(attachment.getFileName());
-        System.out.println(reactionFolder);
+        log.info("{}", attachment);
+        log.info("{}", attachment.getFileName());
+        log.info("{}", reactionFolder);
         Path target = reactionFolder.resolve(attachment.getFileName());
         if (target.toFile().exists()) {
-            System.out.println("already handled");
+            log.info("already handled");
         } else {
             try {
-                System.out.println("trying hardlinking");
+                log.info("trying hardlinking");
                 java.nio.file.Files.createLink(target, attachment);
             } catch (IOException e) {
-                System.out.println(e.getMessage());
+                log.error(e.getMessage());
                 try {
-                    System.out.println("trying copying");
+                    log.info("trying copying");
                     java.nio.file.Files.copy(attachment, target);
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
