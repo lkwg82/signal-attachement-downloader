aeacdb7063b1ae0c1f928fe30abcec6da96d7357
Lars K.W. Gohlke <lkwg82@gmx.de>
2025-01-03 09:49:41 +0100

    debug

--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ function ok() {
 
   info "testing ... "
   log=$(mktemp)
-  if ! docker run --rm -ti --entrypoint signal-cli -w /tmp signal-cli --version >"$log"; then
+  if ! docker run --rm -t --entrypoint signal-cli -w /tmp signal-cli --version >"$log"; then
     echo "ERROR signal-cli failed" >&2
     sed -e 's#^#\t#' "$log"
     exit 1
@@ -31,7 +31,7 @@ function ok() {
 
   info "exit code testing ... "
   log=$(mktemp)
-  if docker run --rm -ti --entrypoint signal-cli signal-cli -u +49123456 receive >"$log" 2>&1; then
+  if docker run --rm -t --entrypoint signal-cli signal-cli -u +49123456 receive >"$log" 2>&1; then
     echo "ERROR signal-cli failed" >&2
     sed -e 's#^#\t#' "$log"
     exit 1
