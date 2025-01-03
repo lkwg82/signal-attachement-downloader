fe42e3b00315ff1afa1fe57e5f63098dd90147e8
Lars <1891171+lkwg82@users.noreply.github.com>
2025-01-03 14:43:10 +0100

    adds release.sh

---
+++ release.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+set -ex
+
+if [[ -z "$DRY_RUN" ]]; then
+  echo "info: to actual run prepend DRY_RUN=0 to $0"
+  dry_run='echo DRY_RUN '
+else
+  if [[ $DRY_RUN == 0 ]]; then
+    dry_run=""
+  else
+    echo "ERROR: not working"
+    echo "$DRY_RUN"
+    exit 1
+  fi
+fi
+
+if [[ ${#dry_run} == 0 ]]; then
+  ./run.sh
+fi
+
+timestamp=$(date "+%Y%m%d-%H%M%S")
+$dry_run git tag "$timestamp"
+
+signalCliVersion=$(docker history --no-trunc signal-cli | grep -v ^$ | grep SIGNAL_CLI_VERSION | cut -d= -f2 | cut -d\  -f1 | xargs)
+
+docker_assets=$(mktemp)
+
+function tag_and_push(){
+  cat <<EOF >> "$docker_assets"
+\`docker pull $2\` ($(docker images "$1" --format "{{.Size}}"))
+EOF
+  $dry_run docker tag "$1" "$2"
+  $dry_run "pushing $2"
+  $dry_run echo
+  $dry_run docker push "$2"
+
+}
+
+tag_and_push signal-cli lkwg82/signal-attachment-mover:signal-cli-"$signalCliVersion"
+tag_and_push signal-cli lkwg82/signal-attachment-mover:signal-cli
+
+tag_and_push attachment-mover lkwg82/signal-attachment-mover:mover
+tag_and_push attachment-mover lkwg82/signal-attachment-mover:mover-"$timestamp"
+
+function log_commits(){
+  if [[ ${#dry_run} == 0 ]]; then
+    git log --oneline  --pretty=format:"%h: %s" "$(git tag | head -n2 | xargs | sed -e 's| |..|')"
+  else
+    git log --oneline --pretty=format:"%h: %s" "$(git tag | head -n1 )"
+  fi
+}
+
+release="test"
+if [[ ${#dry_run} == 0 ]]; then
+  release="$timestamp"
+  gh release create "$release" --pre-release --generate-notes
+else
+  if gh release list --json tagName | jq -e '.[].tagName | select(. == "'"$release"'")'; then
+    gh release delete "$release" --cleanup-tag --yes \
+    ||  gh release delete "$release" --yes || echo "egal"
+  fi
+  gh release create "$release" --draft --generate-notes
+fi
+
+echo "waiting some time"
+sleep 15
+
+
+body_file="$(mktemp).md"
+gh release view "$release" --json body | jq -r '.body' > "$body_file"
+
+cat << EOF >> "$body_file"
+
+-----------------
+
+$(cat "$docker_assets")
+
+see https://hub.docker.com/r/lkwg82/signal-attachment-mover/tags
+EOF
+gh release edit "$release" --notes-file "$body_file"
+
+if [[ ${#dry_run} == 0 ]]; then
+  gh release edit "$release" --latest
+fi
--- run.sh
+++ run.sh
@@ -40,12 +40,13 @@ function ok() {
   ok
 )
 
+
 #mvnd -Dmaven.repo.local=.m2_repo clean verify
 mkdir -p .m2_repo
-docker build -t attachment-mover-java -f docker/attachment-mover-java/Dockerfile .
+docker build -t attachment-mover -f docker/attachment-mover-java/Dockerfile .
 
 log=$(mktemp)
-if ! docker run --rm -t --entrypoint java attachment-mover-java -jar quarkus-run.jar -h >"$log"; then
+if ! docker run --rm -t --entrypoint java attachment-mover -jar quarkus-run.jar -h >"$log"; then
   echo "ERROR app failed" >&2
   cat "$log"
   exit 1
@@ -56,21 +57,3 @@ if [[ -z "$CI" ]]; then
   docker compose up --build attachment-mover
 fi
 
-if [[ -n $RELEASE ]]; then
-  timestamp=$(date "+%Y%m%d-%H%M%S")
-  git tag "$timestamp"
-
-  signalCliVersion=$(docker history --no-trunc signal-cli | grep -v ^$ | grep SIGNAL_CLI_VERSION | cut -d= -f2 | cut -d\  -f1 | xargs)
-  function tag_and_push(){
-    app="release"
-    docker tag "$1" "$2"
-    info "pushing $2"
-    echo
-    docker push "$2"
-  }
-  tag_and_push signal-cli lkwg82/signal-attachment-mover:signal-cli-"$signalCliVersion"
-  tag_and_push signal-cli lkwg82/signal-attachment-mover:signal-cli
-
-  tag_and_push attachment-mover-java lkwg82/signal-attachment-mover
-  tag_and_push attachment-mover-java lkwg82/signal-attachment-mover:mover-"$timestamp"
-fi
